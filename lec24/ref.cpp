65ee84fc909278f3da724f24f2786065fa300618
Thomas Moon <tmoon@linux-a2.ews.illinois.edu>
2019-04-18 10:40:53 -0500

    lec25

---
+++ lec24/ref.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void swap(int *a, int *b){
+    cout<<"Pass by pointer"<<endl;
+    int temp = *a;
+    //a = a+1000000; //seg fault, but no compile error
+    *a = *b;
+    *b = temp;
+}
+void swap(int &a, int &b){
+    cout<<"Pass by reference"<<endl;
+    int temp = a;
+
+    a = b;
+    b = temp;
+}
+int main()
+{
+    int val1, val2;
+    val1 = 10, val2 = 20;
+    swap(&val1, &val2);
+    cout<<val1<<"\t"<<val2<<endl;
+    
+    val1 = 10, val2 = 20;
+    swap(val1, val2);
+    cout<<val1<<"\t"<<val2<<endl;
+}
